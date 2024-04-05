56af072f539d4fb7a400dbefe7ccd1b27b488d80
Marwan <marwan.yousef@mail.utoronto.ca>
2024-04-05 17:11:11 -0400

    latest

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,39 @@ app.listen(port, () => console.log(`Server started on port ${port}`))
 
 app.use(passportConfig.initialize());
 
+
+const multer = require("multer");
+const mongoose = require("mongoose");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./files");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+
+require("./models/pdfDetails");
+const PdfSchema = mongoose.model("PdfDetails");
+const upload = multer({ storage: storage });
+
+app.post("/upload-files", upload.single("file"), async (req, res) => {
+  console.log(req.file);
+  const title = req.body.title;
+  const fileName = req.file.filename;
+  try {
+    await PdfSchema.create({ title: title, pdf: fileName });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.json({ status: error });
+  }
+});
+
 // Define Routes
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/upload-files', require("./routes/uploadRoutes"))
+// app.use('/api/upload-files', require("./routes/uploadRoutes"))
 app.use('/api/jobSeekersRoutes', require("./routes/jobSeekersRoutes"))
 app.use('/api/jobPostingRoutes', require("./routes/jobPostingRoutes"))
 app.use('/api/employerRoutes', require("./routes/employerRoutes"))
